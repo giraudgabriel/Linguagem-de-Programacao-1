b57f3be0c75822976130f99051122aa7aa85527f
Gabriel Fernandes Giraud <gabriel.giraud@fatec.sp.gov.br>
2019-10-30 22:20:15 -0300

    #CHANGE: Correção do nome da função

--- Exercicios/Struct/ex4.c
+++ Exercicios/Struct/ex4.c
@@ -10,7 +10,7 @@ typedef struct	Aluno {
     char curso[50];
 }	Aluno;
 
-int compararIdade(const void *a, const void *b)
+int compararMedia(const void *a, const void *b)
 {
     float media1 = ((struct Aluno*)a)->media;
     float media2 = ((struct Aluno*)b)->media;
@@ -26,32 +26,34 @@ int main(int argc, char const *argv[])
 
     for (int i = 0; i < 5; i++)
     {
-        //curso
-        printf("Digite o nome do curso do aluno %d: \n",i+1);
-        fflush(stdin);
-        gets(alunos[i].curso);
 
         //nome
         printf("Digite o nome do aluno %d: \n",i+1);
         fflush(stdin);
         gets(alunos[i].nome);
 
+        //curso
+        printf("Digite o nome do curso do aluno %d: \n",i+1);
+        fflush(stdin);
+        gets(alunos[i].curso);
+
         //matricula
         printf("Digite a matricula do aluno %d:\n",i+1);
         scanf("%d",&alunos[i].matricula);
             
         //notas
+        alunos[i].media = 0.0;
         for (int k = 0; k < 3; k++)
         {
-            printf("Digite a nota %d: \n", i+1);
+            printf("Digite a nota %d: \n", k+1);
             scanf("%f",&alunos[i].notas[k]);
             alunos[i].media += alunos[i].notas[k];
             alunos[i].media /= k == 2 ? 3 : 1;
         }
         printf("%s \n",(alunos[i].media>= 6.0 ? "Aprovado": "Reprovado"));
     }
-    qsort(alunos,5,sizeof(Aluno),compararIdade);
-    printf("Maior media : \n Aluno: %s \t Media:%f \n",alunos[0].nome,alunos[0].media);
+    qsort(alunos,5,sizeof(Aluno),compararMedia);
+    printf("Maior media : \n Aluno: %s \t Media:%.2f \n",alunos[4].nome,alunos[4].media);
     
     return 0;
 }
